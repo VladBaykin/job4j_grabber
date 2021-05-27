78abb8565dcf9b8e4b2dd4ee6caf7738339a1cbf
Vlad <vladbaykin@gmail.com>
2021-05-27 17:03:49 +0300

    Trigger codecov

---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        assertEquals(1, new Trigger().someLogic());
+    }
+}
