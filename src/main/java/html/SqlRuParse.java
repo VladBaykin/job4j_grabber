4f2c0add2c18f41b7bd287bf3c876560a6c9a704
Vlad <vladbaykin@gmail.com>
2021-05-31 00:36:38 +0300

    SqlRuParse

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -1,5 +1,6 @@
 package html;
 
+import grabber.Parse;
 import grabber.Post;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,9 +10,10 @@ import org.jsoup.select.Elements;
 import utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     public static void main(String[] args) throws Exception {
         String url = "https://www.sql.ru/forum/job-offers";
         for (int i = 1; i < 6; i++) {
@@ -31,17 +33,39 @@ public class SqlRuParse {
         }
     }
 
-    public static Post load(String url) throws IOException {
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td: row) {
+                Element href = td.child(0);
+                String attrUrl = href.attr("href");
+                posts.add(detail(attrUrl));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String url) {
         Post post = new Post();
         post.setLink(url);
-        Document doc = Jsoup.connect(url).get();
-        List<TextNode> name = doc.select(".messageHeader").get(0).textNodes();
-        post.setName(name.get(0).text().trim());
-        Elements desc = doc.select(".msgBody");
-        post.setDescription(desc.get(1).text());
-        Elements date = doc.select(".msgFooter");
-        String fromFooter = date.get(0).text();
-        post.setCreated(new SqlRuDateTimeParser().parse(fromFooter.substring(0, fromFooter.indexOf("[") - 1)));
+        try {
+            Document doc = Jsoup.connect(url).get();
+            List<TextNode> name = doc.select(".messageHeader").get(0).textNodes();
+            post.setName(name.get(0).text().trim());
+            Elements desc = doc.select(".msgBody");
+            post.setDescription(desc.get(1).text());
+            Elements date = doc.select(".msgFooter");
+            String fromFooter = date.get(0).text();
+            post.setCreated(new SqlRuDateTimeParser().parse(fromFooter.substring(0, fromFooter.indexOf("[") - 1)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return post;
     }
 }
--- src/test/java/html/SqlRuParseTest.java
+++ src/test/java/html/SqlRuParseTest.java
@@ -3,7 +3,6 @@ package html;
 import grabber.Post;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.Month;
 
@@ -12,10 +11,11 @@ import static org.junit.Assert.*;
 public class SqlRuParseTest {
 
     @Test
-    public void load() throws IOException {
+    public void load() {
+        SqlRuParse parse = new SqlRuParse();
         String url =
                 "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
-        Post post = SqlRuParse.load(url);
+        Post post = parse.detail(url);
         assertEquals(LocalDateTime.of(2020, Month.MAY, 13, 21, 58, 0),
                 post.getCreated());
         assertEquals("Лиды BE/FE/senior cистемные аналитики/QA и DevOps, Москва, до 200т.", post.getName());
